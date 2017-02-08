8b3d7d510d646bc4cd7367dc07a2b2c73ef41218
Tomáš Skalický <skalicky.tomas@hotmail.com>
2017-02-08 09:31:11 +0100

    Added error for when movieEntity is null

--- app/src/main/java/com/example/android/popularmoviess1/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/popularmoviess1/MovieDetailActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.android.popularmoviess1.Helpers.IntentItems;
@@ -20,6 +21,8 @@ public class MovieDetailActivity extends AppCompatActivity {
     private TextView mMovieReleaseDateTextView;
     private TextView mMovieSynopsisTextView;
     private ImageView mMoviePosterImageView;
+    private TextView mDetailErrorTextView;
+    private ScrollView mDetailScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +34,38 @@ public class MovieDetailActivity extends AppCompatActivity {
         mMovieReleaseDateTextView = (TextView) findViewById(R.id.tv_movie_releaseDate);
         mMovieSynopsisTextView = (TextView) findViewById(R.id.tv_movie_synopsis);
         mMoviePosterImageView = (ImageView) findViewById(R.id.iv_movie_poster);
+        mDetailErrorTextView = (TextView) findViewById(R.id.tv_detail_error);
+        mDetailScrollView = (ScrollView) findViewById(R.id.sv_movie_detail);
 
         Intent intentThatStartedThisActivity = getIntent();
 
         if(intentThatStartedThisActivity.hasExtra(IntentItems.MOVIE_ENTITY)) {
             MovieEntity movieEntity = intentThatStartedThisActivity.getParcelableExtra(IntentItems.MOVIE_ENTITY);
 
-            mMovieTitleTextView.setText(movieEntity.getOriginalTitle());
-            mMovieRatingTextView.setText(getString(R.string.average_rating) + movieEntity.getUserRating().toString());
-            mMovieReleaseDateTextView.setText(getString(R.string.release_date) + movieEntity.getReleaseDate());
-            mMovieSynopsisTextView.setText(movieEntity.getSynopsis());
-            Picasso.with(this).load(movieEntity.getBackdropImage()).into(mMoviePosterImageView);
+            if(movieEntity != null) {
+                showResults();
+                mMovieTitleTextView.setText(movieEntity.getOriginalTitle());
+                mMovieRatingTextView.setText(getString(R.string.average_rating) + movieEntity.getUserRating().toString());
+                mMovieReleaseDateTextView.setText(getString(R.string.release_date) + movieEntity.getReleaseDate());
+                mMovieSynopsisTextView.setText(movieEntity.getSynopsis());
+                Picasso.with(this).load(movieEntity.getBackdropImage()).into(mMoviePosterImageView);
+            } else {
+                showErrors();
+            }
+
+        } else {
+            showErrors();
         }
+    }
+
+    private void showErrors() {
+        mDetailErrorTextView.setVisibility(View.VISIBLE);
+        mDetailScrollView.setVisibility(View.INVISIBLE);
+    }
+
+    private void showResults() {
+        mDetailErrorTextView.setVisibility(View.INVISIBLE);
+        mDetailScrollView.setVisibility(View.VISIBLE);
     }
 
 }
